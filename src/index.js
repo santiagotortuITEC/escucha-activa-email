c1b51d6c3a45cf93d108e350bbec5ad89779fe6d
ggiuliodori <ggiuliodori@kalateam.com.ar>
2021-08-03 23:23:22 +0000

    add log

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Content-Type", "application/json");
 	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-	res.header("Access-Control-Allow-Headers", "*");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
 	next();
 	});
 
